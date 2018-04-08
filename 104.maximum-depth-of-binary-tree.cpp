5147e7fabcba75054a411a7adc4ef6298a7a309d
Yitao <jstzzyt01@gmail.com>
2018-04-07 23:02:39 -0400

    update 104.maximum-depth-of-binary-tree.cpp

--- 104.maximum-depth-of-binary-tree.cpp
+++ 104.maximum-depth-of-binary-tree.cpp
@@ -8,13 +8,13 @@
  * };
  */
 class Solution {
-public:
-    int maxDepth(TreeNode* root) {
-		if(!root){
-			return 0;
-		}        
-		int maxLeft = maxDepth(root->left);
-		int maxRight = maxDepth(root->right);
-		return max(maxLeft, maxRight)+1;
-    }
+	public:
+		int maxDepth(TreeNode* root) {
+			if(!root){
+				return 0;
+			}        
+			int maxLeft = maxDepth(root->left);
+			int maxRight = maxDepth(root->right);
+			return max(maxLeft, maxRight)+1;
+		}
 };
