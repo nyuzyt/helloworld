254f1fd160f000310270cdd836d944cdf8e2fb48
Yitao <jstzzyt01@gmail.com>
2018-04-10 05:18:38 -0400

    update 69.sqrtx.cpp

--- 69.sqrtx.cpp
+++ 69.sqrtx.cpp
@@ -1,35 +1,37 @@
-class Solution {
-	public:
-		int mySqrt(int x) {
-			if (x==0)
-				return 0;
-			int length=0;
-			int y = x;
-			while(y>0){
-				length++;
-				y/=10;
-			}
-			// printf("%d\n",length);
-			int min=1;
-			int max;
-			for (int i=1;i<(length+1)/2;i++)
-				min*=10;
-			if (length%2==0){
-				max = min * 10;
-				min = max / 4;
-			}else max = min * 4;
-			if (min*min == x) return min;
-			int tmp;
-			while (max-min>1){
-				tmp = (min+max)/2;
-				if (tmp*tmp==x)
-					return tmp;
-				else if (tmp>x/tmp)
-					max=tmp;
-				else
-					min=tmp;
-
-			}
-			return min;
-		}
+class Solution
+{
+public:
+    int mySqrt(int x) {
+        if (x == 0)
+            return 0;
+        int length = 0;
+        int y = x;
+        while (y > 0) {
+            length++;
+            y /= 10;
+        }
+        // printf("%d\n",length);
+        int min = 1;
+        int max;
+        for (int i = 1; i < (length + 1) / 2; i++)
+            min *= 10;
+        if (length % 2 == 0) {
+            max = min * 10;
+            min = max / 4;
+        } else
+            max = min * 4;
+        if (min * min == x)
+            return min;
+        int tmp;
+        while (max - min > 1) {
+            tmp = (min + max) / 2;
+            if (tmp * tmp == x)
+                return tmp;
+            else if (tmp > x / tmp)
+                max = tmp;
+            else
+                min = tmp;
+        }
+        return min;
+    }
 };
