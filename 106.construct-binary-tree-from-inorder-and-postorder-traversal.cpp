32a3e5a222bf0fa07676d61ceebbd7ce14af5008
Yitao <jstzzyt01@gmail.com>
2018-04-11 00:19:56 -0400

    update 106.construct-binary-tree-from-inorder-and-postorder-traversal.cpp

--- 106.construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106.construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -13,23 +13,23 @@ public:
         int length = inorder.size();
         if (length <= 0)
             return NULL;
-        if (length == 1)
-            return new TreeNode(inorder[0]);
+        // if (length == 1)
+        //    return new TreeNode(inorder[0]);
         int rootNum = find(inorder.begin(), inorder.end(), postorder[length - 1]) -
                       inorder.begin();
         TreeNode* root = new TreeNode(inorder[rootNum]);
-        if (rootNum == 0)
-            root->left = NULL;
-        else
-            root->left = buildTree(
-                vector<int>(inorder.begin(), inorder.begin() + rootNum),
-                vector<int>(postorder.begin(), postorder.begin() + rootNum));
-        if (rootNum == length - 1)
-            root->right = NULL;
-        else
-            root->right = buildTree(
-                vector<int>(inorder.begin() + rootNum + 1, inorder.end()),
-                vector<int>(postorder.begin() + rootNum, postorder.end() - 1));
+        /* if (rootNum == 0) */
+        // root->left = NULL;
+        /* else */
+        root->left =
+            buildTree(vector<int>(inorder.begin(), inorder.begin() + rootNum),
+                      vector<int>(postorder.begin(), postorder.begin() + rootNum));
+        /*         if (rootNum == length - 1) */
+        // root->right = NULL;
+        /* else */
+        root->right =
+            buildTree(vector<int>(inorder.begin() + rootNum + 1, inorder.end()),
+                      vector<int>(postorder.begin() + rootNum, postorder.end() - 1));
         return root;
     }
 };
