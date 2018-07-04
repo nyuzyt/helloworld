5df5c31aa6f99e31f2b862a15f5ac8c851487359
Yitao <jstzzyt01@gmail.com>
2018-07-03 20:32:23 -0400

    update 349.intersection-of-two-arrays.cpp

---
+++ 349.intersection-of-two-arrays.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        int length1 = nums1.size();
+        int length2 = nums2.size();
+        vector<int> result;
+        if (length1 == 0 || length2 == 0)
+            return result;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        nums1.erase(unique(nums1.begin(), nums1.end()), nums1.end());
+        nums2.erase(unique(nums2.begin(), nums2.end()), nums2.end());
+        length1 = nums1.size();
+        length2 = nums2.size();
+        int i, j;
+        i = j = 0;
+        while (i < length1 && j < length2)
+            if (nums1[i] < nums2[j])
+                i++;
+            else if (nums1[i] > nums2[j])
+                j++;
+            else {
+                result.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+        return result;
+    }
+};
