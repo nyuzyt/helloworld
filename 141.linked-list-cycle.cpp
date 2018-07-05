24c3efbd08f8088f4c6238c2639b216cd2be378e
Yitao <jstzzyt01@gmail.com>
2018-07-04 22:33:31 -0400

    update 141.linked-list-cycle.cpp

---
+++ 141.linked-list-cycle.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *slow, *fast;
+        slow = fast = head;
+        if (!head)
+            return 0;
+        else
+            while (slow->next && fast->next && fast->next->next) {
+                slow = slow->next;
+                fast = fast->next->next;
+                if (fast == slow)
+                    return 1;
+            }
+        return 0;
+    }
+};
